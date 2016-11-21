d4ed10db785a5c2eb8768b777f343831ae688527
Ivan Kozik <ivan@ludios.org>
2016-11-21 10:02:33 +0000

    Remove trailing '.git' from git repository names

--- depmapper.exs
+++ depmapper.exs
@@ -19,7 +19,7 @@ defmodule Mix.DepMapper do
     end
     name = case type do
       :hex -> Atom.to_string(opts.hex)
-      :git -> opts.git |> String.split("/") |> List.last
+      :git -> opts.git |> String.split("/") |> List.last |> String.replace_suffix(".git", "")
       _    -> raise "Dependency is not of type hex or git:\n#{inspect dep, pretty: true, width: 0}"
     end
     git = Path.join(@repos_path, name)
